ee661ed4dc1f9c29219d241b503198fe33ab6440
hazker <zov.zon@gmail.com>
2019-05-01 23:27:29 +0700

    Changes in ClientUI

--- src/main/java/com/server/gradleServer/ClientUI.java
+++ src/main/java/com/server/gradleServer/ClientUI.java
@@ -1,37 +1,36 @@
 package com.server.gradleServer;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
 
 public class ClientUI {
-    //private JButton button1;
+    private JButton button1;
     private JPanel panel1;
-    //private JTextField textField1;
-
-    private String str="lool";
-
+    private JTextField textField1;
+    private JList list1;
 
     public static void main(String[] args){
         JFrame frame=new JFrame("App");
+        Dimension size=new Dimension(800,600);
+        frame.setPreferredSize(size);
         frame.setContentPane(new ClientUI().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-
     }
 
     private ClientUI() {
-        /*button1.addActionListener(new ActionListener() {
+        DefaultListModel dlm=new DefaultListModel();
+        button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                str="Hai";
-                JOptionPane.showMessageDialog(null,"Clicked");
-                JOptionPane.showMessageDialog(null,str);
-                textField1.setText(str);
+                dlm.addElement(textField1.getText());
+                list1.setModel(dlm);
             }
-        });*/
+        });
     }
 }
