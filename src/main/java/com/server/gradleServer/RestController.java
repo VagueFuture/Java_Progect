1cfa85cf6ab4d3161fc6b7b9c3e16cb7c84dd046
Artem <igrokart97@mail.ru>
2019-05-01 18:13:54 +0700

    Test fnction

--- src/main/java/com/server/gradleServer/RestController.java
+++ src/main/java/com/server/gradleServer/RestController.java
@@ -3,4 +3,7 @@ package com.server.gradleServer;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
+    public String test(){
+        return "Hello";
+    }
 }
