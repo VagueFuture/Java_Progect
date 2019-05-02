52777dc23be9c5979da1e8d0ca867d1d224db105
Artem <igrokart97@mail.ru>
2019-05-02 23:57:52 +0700

    New Server Part

--- src/main/java/com/server/gradleServer/MyThread.java
+++ src/main/java/com/server/gradleServer/MyThread.java
@@ -1,17 +1,83 @@
 package com.server.gradleServer;
 
+import org.apache.catalina.Server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyThread implements Runnable {
+    private ServerSocked server;
+    private PrintWriter outMessage;
+    private Scanner inMessage;
+    private static final String HOST = "25.44.20.209";
+    private static final int PORT = 2620;
     Socket Client = null;
-    public void run() {
-    }
-    public MyThread (Socket s){
-        this.Client = s;
+    private static int clients_count = 0;
+
+    public MyThread(Socket socket, ServerSocked server) {
+        try {
+            clients_count++;
+            this.server = server;
+            this.Client = socket;
+            this.outMessage = new PrintWriter(socket.getOutputStream());
+            this.inMessage = new Scanner(socket.getInputStream());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    public Socket getClient() {
-        return this.Client;
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                // сервер отправляет сообщение
+                server.sendMessageToAllClients("Новый участник вошёл в чат!");
+                server.sendMessageToAllClients("Клиентов в чате = " + clients_count);
+                break;
+            }
+
+            while (true) {
+                // Если от клиента пришло сообщение
+                if (inMessage.hasNext()) {
+                    String clientMessage = inMessage.nextLine();
+                    // если клиент отправляет данное сообщение, то цикл прерывается и
+                    // клиент выходит из чата
+                    if (clientMessage.equalsIgnoreCase("##session##end##")) {
+                        break;
+                    }
+                    // выводим в консоль сообщение (для теста)
+                    System.out.println(clientMessage);
+                    // отправляем данное сообщение всем клиентам
+                    server.sendMessageToAllClients(clientMessage);
+                }
+                // останавливаем выполнение потока на 100 мс
+                Thread.sleep(100);
+            }
+        }
+        catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            this.close();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            outMessage.println(msg);
+            outMessage.flush();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void close() {
+        // удаляем клиента из списка
+        server.removeClient(this);
+        clients_count--;
+        server.sendMessageToAllClients("Клиентов в чате = " + clients_count);
     }
 
 }
